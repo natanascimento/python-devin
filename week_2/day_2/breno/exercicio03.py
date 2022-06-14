a62a6d93b8235486b57f4537037261a6efa84a10
Breno <hentgenp@gmail.com>
2022-06-14 20:22:26 -0300

    [BRENO] Update exercicio03.py
    Alternado o nome da var n > num_n para fazer mais sentido.

--- week_2/day_2/breno/exercicio03.py
+++ week_2/day_2/breno/exercicio03.py
@@ -1,10 +1,10 @@
-n = int(input("Digite a quantidade de termos: "))
+num_n = int(input("Digite a quantidade de termos: "))
 
 termo_1 = 0
 termo_2 = 1
 termo_3 = 0
 
-while termo_3 <= n:
+while termo_3 <= num_n:
     print(f"{termo_3}")
 
     termo_3 = termo_1 + termo_2
