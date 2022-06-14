ae8fca38e86aeb378ba3aa2d71054817df90c538
Breno <hentgenp@gmail.com>
2022-06-14 20:19:18 -0300

    [BRENO] Update exercicio03.py
    Adicionado sua lógica ao arquivo.

---
+++ week_2/day_2/breno/exercicio03.py
@@ -0,0 +1,14 @@
+n = int(input("Digite a quantidade de termos: "))
+
+termo_1 = 0
+termo_2 = 1
+termo_3 = 0
+
+while termo_3 <= n:
+    print(f"{termo_3}")
+
+    termo_3 = termo_1 + termo_2
+    termo_1 = termo_2
+    termo_2 = termo_3
+
+
