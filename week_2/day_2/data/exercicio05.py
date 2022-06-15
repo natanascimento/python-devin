1e6ba0e83b602b29857342b2f42acb4edcea3df9
mayconrcampos <maycon.campos@gmail.com>
2022-06-14 22:41:54 -0300

    exercicio05 com filtro por nome e país e menu interativo

---
+++ week_2/day_2/data/exercicio05.py
@@ -0,0 +1,51 @@
+"""
+Crie um método que leia o arquivo e intere sobre ele printando todas
+as linhas no console;
+●Crie um método que armazene os nomes em uma lista utilizando list
+comprehensions;
+"""
+
+with open("nomes.txt") as arquivo:
+    listanomes = [nome for nome in arquivo.readlines()]
+
+
+while True:
+    print("1. Procurar por nome")
+    print("2. Procurar por país")
+    print("3. Listar todos os nomes")
+    print("0. sair")
+    opcao = input("Opção: ")
+
+    if opcao.isnumeric():
+        match opcao:
+            case "0":
+                print("Você saiu")
+                break
+            
+            case "1":
+                nomep = input("Digite o nome pra procurar: ").lower()
+
+                procurandonomes = [nome for nome in listanomes if nome.lower().split(";")[1].startswith(nomep)]
+
+                print(f"Todos nomes que começam com {nomep}")
+                # Printando nomes que começam com B
+                for nome in procurandonomes:
+                    print(nome)
+
+            case "2":
+                cidade = input("Digite o país: ").lower()
+                nomesporpais = [nome for nome in listanomes if nome.lower().split(";")[2].startswith(cidade)]
+                print("Todos da China")
+                # printando nomes de origem Chinesa
+                for nome in nomesporpais:
+                    print(nome)
+            case "3":
+                todosOsNomes = [nome for nome in listanomes]
+                print("Todos os nomes da lista")
+                # Printando todos os nomes
+                for nome in todosOsNomes:
+                    print(nome)
+
+            case _:
+                print("Opção inválida")
+
