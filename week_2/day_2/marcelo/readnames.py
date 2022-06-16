3eb06f841b71712bb668217ade1bd400ed45502b
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-06-15 22:13:34 -0300

    Dict comprehension exercise

--- week_2/day_2/marcelo/readnames.py
+++ week_2/day_2/marcelo/readnames.py
@@ -1,5 +1,12 @@
 from os.path import dirname, abspath, join
 
+def namesreader(path:str):
+    names = [line.split(';')[1] for line in open(path, 'r').readlines()]
+    return names
+def infodict(path:str):
+    info = [dict(id=line.split(';')[0], name=line.split(';')[1], country=line.split(';')[2].split('\n')[0]) for line in open(path, 'r').readlines()]
+    return info
+
 if __name__ == "__main__":
     
     ROOTPATH = dirname(dirname(dirname(dirname(abspath(__file__)))))
@@ -7,11 +14,13 @@ if __name__ == "__main__":
     DAY2PATH = join(WEEK2PATH, "day_2")
     DATAPATH = join(DAY2PATH, "data")
     FAKENAMES = join(DATAPATH, "fake_names_list.txt")
-    print(FAKENAMES)
-    file = open(FAKENAMES, 'r')
+    
+    print(namesreader(FAKENAMES))
+    print(infodict(FAKENAMES))
+    # file = open(FAKENAMES, 'r')
+    # lines = file.readlines()
+    # nameslist = [i.split(';')[1] for i in lines]
+    # print(nameslist)
 
-    lines = file.readlines()
-    nameslist = [i.split(';')[1] for i in lines]
-    print(nameslist)
     # for i in lines:
     #     print(i.split(';')[1])
