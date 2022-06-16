8cf7b97310744afbf70a2a62fcd37b144a90fde9
Breno <hentgenp@gmail.com>
2022-06-15 22:01:35 -0300

    Update exercicio04

--- week_2/day_2/breno/exercicio04.py
+++ week_2/day_2/breno/exercicio04.py
@@ -1,6 +1,7 @@
 f = open("week_2/day_2/data/fake_names_list.txt")
 print(f.read())
 
-y = [x for x in f]
+y = [x.split(";")[1] for x in f] # uma forma de ver só pelo nome
+# y = [x for x in f]
 
 print(y)
