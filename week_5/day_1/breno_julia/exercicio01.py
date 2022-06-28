48fac70715d487c3eaec6879e46fffe1a082ffde
Breno <hentgenp@gmail.com>
2022-06-27 21:46:19 -0300

    [BRENO] Update exercicio01

--- week_5/day_1/breno_julia/exercicio01.py
+++ week_5/day_1/breno_julia/exercicio01.py
@@ -1,9 +1,41 @@
 class Agricultor:
     def __init__(self, nome, idade):
-        self.nome = nome
+        self.__nome = nome
         self.__idade = idade
+        self.__ferramenta = None
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def idade(self):
+        return self.__idade
+    
+    @property
+    def ferramenta(self):
+        return self.__ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self.__ferramenta = ferramenta
 
 
 class Ferramenta:
-    def __init__(self) -> None:
-        
+    def __init__(self, tipo):
+        self.__tipo = tipo
+
+    @property
+    def tipo_ferramenta(self):
+        return self.__tipo
+    
+    def usando_ferramenta(self):
+        print(f'Ferramenta {self.__tipo} está sendo usada!')
+
+agricultor_1 = Agricultor('Breno', 20)
+
+enxada = Ferramenta('Enxada')
+
+agricultor_1.ferramenta = enxada
+
+agricultor_1.ferramenta.usando_ferramenta()
