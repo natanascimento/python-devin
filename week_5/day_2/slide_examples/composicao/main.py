009deb82c9d028e78a37a60e0fac642e49e65ad4
Natan Nascimento <natan.nascimento@xpi.com.br>
2022-06-28 21:15:44 -0300

    feat: added class composition

---
+++ week_5/day_2/slide_examples/composicao/main.py
@@ -0,0 +1,38 @@
+class Cliente:
+
+    def __init__(self, nome: str) -> None:
+        self.nome = nome
+        self.enderecos = []
+        self.class_name = __class__.__name__
+
+    def cadastrar_endereco(self, cidade: str, estado: str):
+        self.enderecos.append(Endereco(cidade=cidade,
+                                       estado=estado))
+
+    def listar_enderecos(self):
+        for endereco in self.enderecos:
+            print(f"O Cliente {self.nome} reside em {endereco.cidade}-{endereco.estado}")
+
+    def __del__(self):
+        print(f"REMOVENDO {self.nome} e {self.enderecos}")
+
+
+class Endereco:
+
+    def __init__(self, cidade: str, estado: str):
+        self.cidade = cidade
+        self.estado = estado
+
+    def __del__(self):
+        print(f"REMOVENDO CLASSE ENDERECO COM {self.cidade} e {self.estado}")
+
+
+if __name__ == "__main__":
+    cliente = Cliente(nome="Natan")
+    #cliente.cadastrar_endereco(cidade="Aracaju",
+                               #estado="SE")
+    cliente.listar_enderecos()
+
+    print("NOME DA MINHA CLASSE", cliente.class_name)
+
+    del cliente
