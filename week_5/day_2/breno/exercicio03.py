fe0ab1c3775c80cdccdd655fa7bc2269aabb4f8d
Breno <hentgenp@gmail.com>
2022-06-28 21:36:27 -0300

    [BRENO] Update exercicio03

--- week_5/day_2/breno/exercicio03.py
+++ week_5/day_2/breno/exercicio03.py
@@ -1,22 +1,28 @@
 class Funcionario:
 
-    def __init__(self, funcao: str):
+    def __init__(self, nome: str, funcao: str):
+        self.nome = nome
         self.funcao = funcao
-        self.funcionarios = []
-    
-    def cadastrar_funcionario(self, nome):
-        self.funcionarios.append(Empresa(nome = nome))
-
-    def listar_funcionarios(self):
-        for funcionario in self.funcionarios:
-            print(f"Funcionário: {funcionario.nome}, Função: {self.funcao}")
 
 
 class Empresa:
 
     def __init__(self, nome: str):
         self.nome = nome
+        self.funcionarios = []
+    
+    def cadastrar_funcionario(self, nome, funcao):
+        self.funcionarios.append(Funcionario(nome = nome, funcao = funcao))
 
-func_1 = Funcionario('Dev')
-func_1.cadastrar_funcionario('Breno')
-func_1.listar_funcionarios()
+    def listar_funcionarios(self):
+        for funcionario in self.funcionarios:
+            print(f"Funcionário: {funcionario.nome}, Função: {funcionario.funcao}, Empresa: {self.nome}")
+
+
+empresa_1 = Empresa('Conecta')
+
+empresa_1.cadastrar_funcionario('Breno', '0')
+empresa_1.cadastrar_funcionario('Maycon', '1')
+empresa_1.cadastrar_funcionario('Natan', '2')
+
+empresa_1.listar_funcionarios()
